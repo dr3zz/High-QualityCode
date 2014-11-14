ef8739749289b4276a2ad5cd415504d0b615113e
Yavor Todorov <dr3zz@users.noreply.github.com>
2014-11-14 10:47:56 +0200

    Homework Defensive Programming

---
+++ Exceptions/ExamResult.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+public class ExamResult
+{
+    public ExamResult(int grade, int minGrade, int maxGrade, string comments)
+    {
+        if (grade < 0)
+        {
+            throw new ArgumentOutOfRangeException("grade", "Grade cannot be negative");
+        }
+
+        if (minGrade < 0)
+        {
+            throw new ArgumentOutOfRangeException("minGrade", "Min grade cannot be negative");
+        }
+
+        if (maxGrade <= minGrade)
+        {
+            throw new ArgumentOutOfRangeException("maxGrade", "Max grade should be greater than Min grade");
+        }
+
+        if (string.IsNullOrEmpty(comments))
+        {
+            throw new ArgumentException("comments cannot be empty");
+        }
+
+        this.Grade = grade;
+        this.MinGrade = minGrade;
+        this.MaxGrade = maxGrade;
+        this.Comments = comments;
+    }
+
+    public int Grade { get; private set; }
+
+    public int MinGrade { get; private set; }
+
+    public int MaxGrade { get; private set; }
+
+    public string Comments { get; private set; }
+}
